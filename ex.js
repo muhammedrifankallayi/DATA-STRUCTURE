c11dce4fab256f148f6406698b301ddd30b7d83c
muhammed rifan <rifanmuhammed173@gmail.com>
2023-07-26 18:46:26 +0530

    gap print question

--- ex.js
+++ ex.js
@@ -17,3 +17,42 @@ const user = [
         hobby: ['running', 'football', 'sports']
     },
 ];
+
+// for(let i=0;i<user.length;i++){
+//     setTimeout(() => {
+//       console.log(user[i].name);
+//     }, 3000)
+    
+//     user[i].hobby.forEach((val)=>{
+//         setTimeout(() => {
+//             console.log(val);
+//         }, 6000);
+//     })
+   
+// }
+
+const printNumbersSync =async ()=> {
+  for(let i=0;i<user.length;i++){
+      await new Promise(resolve => {
+        setTimeout(() => {
+          console.log(user[i].name);
+         
+          resolve();
+        }, 1000);
+      });
+      for(let j=0;j<user[i].hobby.length;j++){
+        await new Promise(resolve => {
+          setTimeout(() => {
+            console.log("hobby:"+user[i].hobby[j]);
+           
+            resolve();
+          }, 1000);
+        });
+      }
+    }
+  }
+  
+  printNumbersSync();
+  
+
+
